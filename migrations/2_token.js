90e1884f56b6306477a7803b7a5964e44fb39505
ts 21 <shoukitsuda0310@gmail.com>
2020-12-20 17:22:10 +0900

    fix

--- migrations/2_token.js
+++ migrations/2_token.js
@@ -1,7 +1,9 @@
 var MyToken = artifacts.require("MyToken");
 
 module.exports = function(deployer) {
-    // Use deployer to state migration tasks.
+    //Todo:総発行数を設定
     const initialSupply = "200000000000000";
-    deployer.deploy(MyToken,initialSupply);
+    deployer.deploy(MyToken,initialSupply,{
+        gas:3000000
+    });
 };
